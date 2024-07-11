72e4d6e0983a2abf3816547a77fa7556b69d0b18
rana-ji <akrana9599@gmail.com>
2024-07-11 21:11:34 +0530

    cors error

--- server/controllers/storefront.js
+++ server/controllers/storefront.js
@@ -15,11 +15,6 @@ export const createVariant = async (req, res) => {
     const { client: graphqlClient } = await clientProvider.offline.graphqlClient({
       shop: res.locals.user_shop,
     });
-    // const store = graphqlClient.session.shop;
-    
-    // const { client: restClient } = await clientProvider.offline.restClient({
-    //   shop: store,
-    // });
 
     const data = await graphqlClient.request(
       `mutation productVariantCreate($input: ProductVariantInput!) {
@@ -51,34 +46,6 @@ export const createVariant = async (req, res) => {
         }
       }
     )
-    console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&",data)
-    // const productResponse = await restClient.get({
-    //   path: `/products/${productId}.json`,
-    // });
-
-    // if (!productResponse || !productResponse.body || !productResponse.body.product) {
-    //   return res.status(404).json({ error: "Product not found" });
-    // }
-
-    // const { product } = productResponse.body;
-    // const { options, image } = product;
-
-    // const variantData = {
-    //   price,
-    //   image: image?.src || null,
-    //   inventory_policy: "continue",
-    // };
-
-    // options.forEach((_, index) => {
-    //   variantData[`option${index + 1}`] = `cal-${new mongoose.Types.ObjectId().toString()}`;
-    // });
-
-    // const variantResponse = await restClient.post({
-    //   path: `/products/${productId}/variants`,
-    //   data: {
-    //     variant: variantData,
-    //   },
-    // });
 
     if (data.errors) {
       return res.status(500).json({ error: "Failed to create variant" });
